b10d59573fb199ba9ac485bf6560e3764105b697
Dani lukman <danilukman2206@gmail.com>
2023-06-06 22:41:48 +0700

    register

--- lib/views/register.dart
+++ lib/views/register.dart
@@ -16,83 +16,85 @@ class _RegisterPageState extends State<RegisterPage> {
   late String role;
   final _formKey = GlobalKey<FormState>();
   @override
+  // ignore: override_on_non_overriding_member
   bool _passwordVisible = false;
   void initState() {
     _passwordVisible = false;
     super.initState();
   }
+
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Register')),
-        body: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  obscureText: false,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Masukan Nama Lengkap';
-                    }
-                    return null;
-                  },
-                  maxLines: 1,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      prefixIcon: const Icon(Icons.person),
-                      labelText: 'Masukan Nama',
-                      hintText: 'Masukan Nama'),
-                  onChanged: (value) {
-                    setState(() {
-                      nama = value;
-                    });
-                  },
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  obscureText: false,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Masukan Email';
-                    }
-                    return null;
-                  },
-                  maxLines: 1,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      prefixIcon: const Icon(Icons.email),
-                      labelText: 'Masukan Email',
-                      hintText: 'Masukan Email'),
-                  onChanged: (value) {
-                    setState(() {
-                      email = value;
-                    });
-                  },
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
+      appBar: AppBar(title: const Text('Register')),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                obscureText: false,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Masukan Nama Lengkap';
+                  }
+                  return null;
+                },
+                maxLines: 1,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    prefixIcon: const Icon(Icons.person),
+                    labelText: 'Masukan Nama',
+                    hintText: 'Masukan Nama'),
+                onChanged: (value) {
+                  setState(() {
+                    nama = value;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                obscureText: false,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Masukan Email';
+                  }
+                  return null;
+                },
+                maxLines: 1,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    prefixIcon: const Icon(Icons.email),
+                    labelText: 'Masukan Email',
+                    hintText: 'Masukan Email'),
+                onChanged: (value) {
+                  setState(() {
+                    email = value;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
                 obscureText: !_passwordVisible,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Masukan Password';
-                    }
-                    return null;
-                  },
-                  maxLines: 1,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Masukan Password';
+                  }
+                  return null;
+                },
+                maxLines: 1,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
                     suffixIcon: GestureDetector(
                       onTap: () {
                         setState(() {
@@ -103,71 +105,71 @@ class _RegisterPageState extends State<RegisterPage> {
                           ? Icons.visibility
                           : Icons.visibility_off),
                     ),
-                      prefixIcon: const Icon(Icons.key),
-                      labelText: 'Masukan Password',
-                      hintText: 'Masukan Password'),
-                  onChanged: (value) {
-                    setState(() {
-                      password = value;
-                    });
-                  },
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Masukan Nomor Hp';
+                    prefixIcon: const Icon(Icons.key),
+                    labelText: 'Masukan Password',
+                    hintText: 'Masukan Password'),
+                onChanged: (value) {
+                  setState(() {
+                    password = value;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                obscureText: false,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Masukan Nomor Hp';
+                  }
+                  return null;
+                },
+                maxLines: 1,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    prefixIcon: const Icon(Icons.phone),
+                    labelText: 'Masukan Nomor Hp',
+                    hintText: 'Masukan Nomor Hp'),
+                onChanged: (value) {
+                  setState(() {
+                    noHp = value;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              InkWell(
+                  onTap: () async {
+                    if (_formKey.currentState!.validate()) {
+                      await HttpService.register(
+                          email, password, nama, noHp, context);
                     }
-                    return null;
                   },
-                  maxLines: 1,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
+                  child: Container(
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 1, vertical: 10),
+                    child: const Center(
+                      child: Text(
+                        "Register",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.white),
                       ),
-                      prefixIcon: const Icon(Icons.phone),
-                      labelText: 'Masukan Nomor Hp',
-                      hintText: 'Masukan Nomor Hp'),
-                  onChanged: (value) {
-                    setState(() {
-                      noHp = value;
-                    });
-                  },
-                ),
-                
-                SizedBox(
-                  height: 20,
-                ),
-                InkWell(
-                    onTap: () async {
-                      if (_formKey.currentState!.validate()) {
-                        await HttpService.register(
-                            email, password, nama, noHp, context);
-                      }
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.symmetric(
-                          horizontal: 1, vertical: 10),
-                      child: const Center(
-                        child: Text(
-                          "Register",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, color: Colors.white),
-                        ),
-                      ),
-                      height: 50,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                          color: Color.fromRGBO(9, 107, 199, 1),
-                          borderRadius: BorderRadius.circular(10)),
-                    ))
-              ],
-            ),
+                    ),
+                    height: 50,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(9, 107, 199, 1),
+                        borderRadius: BorderRadius.circular(10)),
+                  ))
+            ],
           ),
+        ),
       ),
-        );
+    );
   }
 }
